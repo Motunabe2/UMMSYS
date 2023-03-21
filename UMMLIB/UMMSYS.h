2f8d2613dc5597cd5cbadf3d9600cef76c5067b9
Motunabe2 <85874427+Motunabe2@users.noreply.github.com>
2023-03-21 20:07:30 +0900

    Create UMMSYS.h

---
+++ UMMLIB/UMMSYS.h
@@ -0,0 +1,67 @@
+/****************************************************************************
+ *
+ * UMMSYStem
+ *
+ * Copyright (c) Motunabe2
+ *
+ * Library  : UMMLIB
+ * Module   : System Body
+ * File     : UMMSYS.h
+ *
+ ****************************************************************************/
+
+
+  /* 多重定義防止					*/
+  /* Prevention of redefinition	*/
+#pragma once
+
+/***************************************************************************
+ *      マクロ定数宣言
+ *      Macro
+ ***************************************************************************/
+#define MAX_SCENE_NUM 256
+#define SCREEN_SIZE_H 480
+#define SCREEN_SIZE_W 640
+
+ /***************************************************************************
+  *      インクルード
+  *      include
+  ***************************************************************************/
+#include "DxLib.h"
+#include "Sample.h"
+
+class Button;
+class SceneProgramer;
+
+class UMMSYS
+{
+public:
+	UMMSYS();//コンストラクタ
+	~UMMSYS();//デコンストラクタ
+
+private:
+	int BOOT();
+	int window_size_H;
+	int window_size_W;
+
+	unsigned short scene_status;
+	//シーンポインタ
+	SceneProgramer* ScenePP;//シーンプログラマ
+};
+
+#define SCENE_Sample 0
+
+class SceneProgramer
+{
+public:
+	SceneProgramer();//コンストラクタ
+	int SceneExec(int tick);//シーンの実行
+	int SceneChange(int CheneNum);
+  
+private:
+	int RunningScene;
+	int SceneID;
+
+	//シーンポインタ宣言
+	Scene_Sample* Sample;
+};
