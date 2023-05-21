950693cfa5b1a4e258c4bfa325d753825268bfe5
Motunabe2 <85874427+Motunabe2@users.noreply.github.com>
2023-05-22 02:14:47 +0900

    Add files via upload

--- UMMLIB/UMMGGC.cpp
+++ UMMLIB/UMMGGC.cpp
@@ -16,13 +16,38 @@
   ***************************************************************************/
 #include "UMMGGC.h"
 
-int MonoLisic::Start()
+int MonoLithic :: Execute()
+{
+	retVal = 0;
+	if (StartFL == true) 
+	{
+		retVal = Start();
+		StartFL = false;
+	}
+	else { retVal = Update(); }
+	Components();//コンポーネント実行
+	return retVal;//正常終了
+}
+
+int MonoLithic::Constructer()
 {
 
 	return 0;//正常終了
 }
 
-int MonoLisic::Update()
+int MonoLithic::Start()
+{
+
+	return 0;//正常終了
+}
+
+int MonoLithic::Update()
+{
+
+	return 0;//正常終了
+}
+
+int MonoLithic::Components()
 {
 
 	return 0;//正常終了
@@ -42,13 +67,6 @@ int BoxCollider2D::Update()
 }
 
 
-Transform2D :: Transform2D()
-{
-
-}
-
-
-
 int Vector2 :: Start()
 {
 
@@ -61,7 +79,76 @@ int Vector2::Update()
 	return 0;//正常終了
 }
 
-GameObject :: GameObject()
+
+
+
+int WindowAbyss :: Start()
 {
-	X = 0;
+
+	return 0;
 }
+int WindowAbyss :: Update()
+{
+	if (0 < WAX && WAX < 480)
+	{
+		if (0 < WAY && WAY < 640)
+		{
+			CheckHit = false;
+		}
+	}
+	else
+	{
+		CheckHit = true;
+	}
+	return 0;
+}
+
+#if USE_DXLib
+int Sprite::Start()
+{
+	X = 0;//横位置初期化
+	Y = 0;//縦位置初期化
+	Display = false;//初期状態では表示しない
+	
+	GHL = LoadGraph(FName);//画像の読み込み
+
+	return 0;//正常終了
+}
+
+int Sprite :: Draw()
+{
+	
+	return 0;//正常終了
+}
+
+int Sprite::FileName(const char FN[256])
+{
+	//*FName = FN;
+	return 0;//正常終了
+}
+
+
+int AssetManager :: LoadAsset(const char FileName[],int FileType)
+{
+	HandleList[2] = 0;
+	if (FileType == ASSET_TYPE_IMAGE)
+	{
+		return LoadGraph(FileName);
+	}
+	else if (FileType == ASSET_TYPE_SOUND)
+	{
+		work = LoadSoundMem(FileName);
+		
+	}
+	else { return -10;/*エラー : ファイルタイプ選択が間違っている*/ }
+}
+
+int AssetManager::Serch_No_UseHList()
+{
+	for (int cnt = 0; cnt < HandleSize; cnt++)
+	{
+		if (HandleList[cnt] == false) { return cnt; }
+	}
+}
+#endif
+
--- UMMLIB/UMMGGC.h
+++ UMMLIB/UMMGGC.h
@@ -14,33 +14,119 @@
  /* Prevention of redefinition	*/
 #pragma once
 
+/***************************************************************************
+ *      マクロ定数宣言
+ *      Macro
+ ***************************************************************************/
+#define USE_DXLib true//Dxライブラリを使用する
 
-class MonoLisic
+//クラス判別用定数
+#define TYPE_ACTOR 125
+#define TYPE_COMPONENTS 126
+#define TYPE_SCENE 127
+
+ /***************************************************************************
+  *      インクルード
+  *      include
+  ***************************************************************************/
+#if USE_DXLib 
+#include "DxLib.h"
+#endif
+
+
+/**
+* コンポーネントやシーン等のオブジェクト用の基底クラスです。
+*/
+class MonoLithic//コンポーネント
 {
+//private:
 public:
+	bool StartFL;
+	MonoLithic() 
+	{
+		StartFL = true; 
+		Constructer();
+	}
+	virtual int Constructer();
+private : int retVal;
+public:
+	int Execute();
 	virtual int Start();
 	virtual int Update();
+	virtual int Components();
 };
 
-class BoxCollider2D : public MonoLisic
+class Actor :public MonoLithic
 {
 public:
-	BoxCollider2D() { Start(); }
+	const unsigned char Type = TYPE_ACTOR;
+};
+class Components :public MonoLithic
+{
+public:
+	const unsigned char Type = TYPE_COMPONENTS;
+};
+class Scene :public MonoLithic
+{
+public:
+	const unsigned char Type = TYPE_SCENE;
+};
+
+class Actor2
+{
+public:
+	enum
+	{
+		EActive,
+		EPaused,
+		EDead
+	};
+public:
+	void AddComponent(MonoLithic* Comp);
+	void RemoveComponent(MonoLithic* Comp);
+
+	//void Update();
+
+	void UpdateAllComponent();
+
+	virtual int Start();
+	virtual int Update();
+	//コンポーネント
+
+private:
+	//std::unordered_set<MonoLisic*>mComponents:
+};
+
+
+
+class BoxCollider2D : public Components
+{
+public:
+	//BoxCollider2D() { Start(); }
 	int Start();
 	int Update();
 	bool CheckHit;
-	int X, Y;
+	int BoxX, BoxY;
+	int SizeXS, SizeYS, SizeXE, SizeYE;
 };
 
-class Transform2D
+class Transform2D : public Components
 {
 public:
-	Transform2D();
 	int X, Y;
 	int Rotate;
 };
 
-class Vector2 : public MonoLisic
+class TransformP2D : public Components
+{
+public:
+	int X;
+	int Y;
+	int Rotate;
+	int Direction;
+};
+
+class Vector2 : public Components
 {
 public:
 	int Update();
@@ -51,8 +137,66 @@ public:
 	float Y;
 };
 
-class GameObject : public MonoLisic , Transform2D
+class WindowAbyss : public Components
 {
 public:
-	GameObject();
+	int Start();
+	int Update();
+	int WAX, WAY;
+	int WASizeX, WASizeY;
+	bool CheckHit;
 };
+
+class PCamera : public Components
+{
+public:
+	int Start();
+	int Update();
+
+};
+
+#if USE_DXLib
+class Sprite : public Components
+{
+public:
+	int Start();
+	int Draw();
+	int FileName(const char FN[256]);
+
+	char FName[256];
+	bool Display;
+	int X;
+	int Y;
+
+private:
+	int GHL;
+};
+
+
+#define ASSET_TYPE_IMAGE 50
+#define ASSET_TYPE_SOUND 60
+
+class AssetManager
+{
+public:
+	AssetManager(int HLSize = 256)
+	{
+		HandleList = new int[HLSize];
+		UsedHandleList = new bool[HLSize];
+		HandleSize = HLSize;
+		for (int cnt = 0; cnt < HLSize; cnt++) 
+		{
+			UsedHandleList[cnt] = false;
+		}
+	}
+public:
+	int LoadAsset(const char FileName[],int FileType);
+	int DeleteAsset(int Handle);
+	int* HandleList;
+private:
+	int work;
+	bool* UsedHandleList;
+	int HandleSize;
+	int Serch_No_UseHList();
+};
+#endif
